4d88623385fd85c2438e846cf95f627d31d73d7e
shira <shira79@users.noreply.github.com>
2022-06-05 17:11:43 +0900

    html表示

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
+
+const tmplPath = "src/template/"
 
 // グローバル変数
 var e = createMux()
@@ -29,6 +33,26 @@ func createMux() *echo.Echo {
 	return e
 }
 
+// 一覧表示
 func articleIndex(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	// 空のインターフェースは、任意の型の値を保持できる。
+	data := map[string]interface{}{
+		"Message" : "Hello, World!",
+		"Now"     : time.Now(),
+	}
+	  return render(c, "article/index.html", data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	// 生成された HTML をバイトデータとして受け取る
+    b, err := htmlBlob(file, data)
+    if err != nil {
+      return c.NoContent(http.StatusInternalServerError)
+    }
+    return c.HTMLBlob(http.StatusOK, b)
+}
+
+// テンプレートとデータからHTMLをバイトデータとして生成
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+    return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
 }
